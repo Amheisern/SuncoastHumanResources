7a33fe1c5bf497fab3f1a6fa63a7ca50dbd5a3eb
lodashreload <icarusjapan@gmail.com>
2021-12-18 22:20:30 -0500

    Added a database for the first time guided

--- Program.cs
+++ Program.cs
@@ -15,6 +15,38 @@ namespace SuncoastHumanResources
         }
     }
 
+    class EmployeeDatabase
+    {
+        // Keep a *private* copy of the employee list.
+        //
+        // We make this private since we don't want code from
+        // outside this class to have access to it. All access
+        // to this information comes through the methods of the
+        // class.
+        private List<Employee> employees = new List<Employee>();
+
+        // Get a list of all the employees
+        public List<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+
+        // Get a list of all the employees
+        public void AddEmployee(Employee newEmployee)
+        {
+            employees.Add(newEmployee);
+        }
+
+        // Given a name as a string, look through the list of
+        // employees. If we find one with a matching name, return
+        // the employee. If nothing is found, return a null.
+        public Employee FindOneEmployee(string name)
+        {
+            // to null which will indicate no match found
+            Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name == name);
+            return foundEmployee;
+        }
+    }
     class Program
     {
         static void DisplayGreeting()
@@ -54,13 +86,16 @@ namespace SuncoastHumanResources
         static void Main(string[] args)
         {
             // Our list of employees
-            var employees = new List<Employee>();
+            // var employees = new List<Employee>();
 
-
-            DisplayGreeting();
+            // Our databased 
+            var database = new EmployeeDatabase();
 
             // Should we keep showing the menu?
             var keepGoing = true;
+
+            DisplayGreeting();
+
 
             // While the user hasn't said QUIT yet
             while (keepGoing)
@@ -84,7 +119,8 @@ namespace SuncoastHumanResources
                     // to null which will indicate no match found
                     //like starting from zero with indexes
                     //Employee foundEmployee = null;
-                    Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name == name);
+                    Employee foundEmployee = database.FindOneEmployee(name);
+                    //Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name == name);
                     // Go through all the employees
                     // foreach (var employee in employees)
                     // {
@@ -107,6 +143,7 @@ namespace SuncoastHumanResources
                 }
                 else if (choice == "S")
                 {
+                    var employees = database.GetAllEmployees();
                     // Loop through each employee: Could replace with a LINQ function
                     foreach (var employee in employees)
                     {
@@ -125,7 +162,8 @@ namespace SuncoastHumanResources
                     employee.Department = PromptForInteger("What is your department number? ");
                     employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
                     // Add it to the list
-                    employees.Add(employee);
+                    database.AddEmployee(employee);
+                    // employees.Add(employee);
                 }
                 // end of the `while` statement
 
