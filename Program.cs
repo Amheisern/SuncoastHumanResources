43c2474bef7078e47e5a9d2c87bd4197bfb26d82
lodashreload <icarusjapan@gmail.com>
2021-12-21 11:25:31 -0500

    organizing code based on location

--- Program.cs
+++ Program.cs
@@ -1,60 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SuncoastHumanResources
 {
-    class Employee
-    {
-        public string Name { get; set; }
-        public int Department { get; set; }
-        public int Salary { get; set; }
-        public int MonthlySalary()
-        {
-            return Salary / 12;
-        }
-    }
-
-    class EmployeeDatabase
-    {
-        // Keep a *private* copy of the employee list.
-        //
-        // We make this private since we don't want code from
-        // outside this class to have access to it. All access
-        // to this information comes through the methods of the
-        // class.
-        private List<Employee> employees = new List<Employee>();
-
-        // Get a list of all the employees
-        public List<Employee> GetAllEmployees()
-        {
-            return employees;
-        }
-
-        // Get a list of all the employees
-        public void AddEmployee(Employee newEmployee)
-        {
-            employees.Add(newEmployee);
-        }
-
-        // Given a name as a string, look through the list of
-        // employees. If we find one with a matching name, return
-        // the employee. If nothing is found, return a null.
-        public Employee FindOneEmployee(string name)
-        {
-            // to null which will indicate no match found
-            Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name.ToUpper().Contains(name.ToUpper()));
-            return foundEmployee;
-        }
-        // method for removing a employee from the list
-        public void RemoveEmployee(Employee employee)
-        {
-            employees.Remove(employee);
-
-        }
-
-
-    }
     class Program
     {
         static void DisplayGreeting()
